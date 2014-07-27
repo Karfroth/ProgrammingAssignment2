bc294f1b42320f5b48685df11a29bb3445336740
Woosang Lee <recollect12@gmail.com>
2014-07-27 17:59:04 +0900

    Comment Edited
    Signed-off-by: Woosang Lee <recollect12@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,26 @@
 ## makeCacheMatrix and cacheSolve are functions for caching the inverse matrix
 
 
-## makeCacheMatrix function contains 4 functions.						#
-## use "(variable) <- makeCacheMatrix()" 								#
-## or "(variable) <- makeCacheMatrix('variable that contain matrix')"	#
-## $set will set matrix													#
-## $get will show you cached matrix										#
-## You can set inverse matrix yourself by using $setsolve				#
-## You can get cached inverse matrix by $getsolve						#
+## makeCacheMatrix function contains 4 functions.
+##
+## use "(variable) <- makeCacheMatrix()" 								
+## or "(variable) <- makeCacheMatrix('variable that contain matrix')"	
+## $set will set matrix in makeCacheMatrix function
+## $get will show you cached matrix in makeCacheMatrix function
+## You can set inverse matrix yourself by using $setsolve				
+## You can get cached inverse matrix by $getsolve
+## Check Examples in tail of this script
 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x, ...) {
   m<-NULL
   set<-function(y, ...){
-    x<<-matrix(y, ...)
+    if (is.matrix(y)==T){
+      x<<-y
+    }
+    else{
+      x<<-matrix(y, ...)
+    }
     m<<-NULL
   }
   get<-function() {
@@ -47,7 +54,7 @@ cacheSolve <- function(x, ...) {
   ## Calculate the Determinant of a Matrix.
   ## if det(x)==0, than this matrix can`t have inverse matrix.
   if(det(data)==0){
-    message("This matrix doesn`t have inverse matrix.")
+    stop("This matrix doesn`t have inverse matrix.")
   }
   
   ## if det(x)!=0, get solve.
@@ -57,3 +64,54 @@ cacheSolve <- function(x, ...) {
     m
   }
 }
+
+###############################################
+###############################################
+############Example 1
+#
+#> example.matrix<-makeCacheMatrix()
+#> example.matrix$set(1:4, nrow=2, ncol=2)
+#> example.matrix$get()
+#     [,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> cacheSolve(example.matrix)
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#
+###############################################
+############Example 2
+#
+#> example.matrix2<-makeCacheMatrix()
+#> example.matrix2$set(1:9, nrow=3, ncol=3)
+#> example.matrix2$get()
+#     [,1] [,2] [,3]
+#[1,]    1    4    7
+#[2,]    2    5    8
+#[3,]    3    6    9
+#> cacheSolve(example.matrix2)
+#Error in cacheSolve(example.matrix2) : 
+#  This matrix doesn`t have inverse matrix.
+#
+###############################################
+############Example 3
+#
+#> example1<-matrix(1:4, nrow=2, ncol=2)
+#> example.matrix3<-makeCacheMatrix()
+#> example.matrix3$set(example1)
+#> example.matrix3$get()
+#     [,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> cacheSolve(example.matrix3)
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> example.matrix3$getsolve()
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#
+###############################################
+###############################################
